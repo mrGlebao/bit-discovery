d3c1d0bf2da0ed80c1d35fd4f426c193d125163c
mrGlebao <zaborovskij.gleb@gmail.com>
2017-05-06 23:52:25 +0300

    Added version logic
    Still have some troubles with version property in metadata map

--- gateway/src/main/java/ru/sbt/ds/filters/route/VersionFilter.java
+++ gateway/src/main/java/ru/sbt/ds/filters/route/VersionFilter.java
@@ -1,5 +1,6 @@
 package ru.sbt.ds.filters.route;
 
+import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 import com.netflix.zuul.ZuulFilter;
@@ -52,13 +53,29 @@ public class VersionFilter extends ZuulFilter {
     private String getAppropriateRoutingUrl(RequestContext context) {
         List<Application> instances = eurekaClient.getApplications().getRegisteredApplications();//getInstancesById("photo-service");
 
-        Application application = null;
+        List<InstanceInfo> photoServices = null;
         for (Application app : instances) {
             if ("photo-service".equals(app.getName().toLowerCase())) {
-                application = app;
+                photoServices = app.getInstances();
+                break;
             }
         }
-        return application == null ? null : application.getInstances().get(0).getHomePageUrl();
+
+        //Choose right url
+        if(photoServices == null || photoServices.isEmpty()) {
+            // There may be some failover logic
+            return EMPTY_STRING;
+        }
+        InstanceInfo maxVersionPhotoService = photoServices.get(0);
+        int version = Integer.valueOf(maxVersionPhotoService.getMetadata().get("version"));
+        for(InstanceInfo photoService : photoServices){
+            int newVersion = Integer.valueOf(photoService.getMetadata().get("version"));
+            if(newVersion > version) {
+                maxVersionPhotoService = photoService;
+                version = Integer.valueOf(maxVersionPhotoService.getMetadata().get("version"));
+            }
+        }
+        return maxVersionPhotoService.getHomePageUrl();
     }
 
     private String formQuery(RequestContext context) {
@@ -69,3 +86,4 @@ public class VersionFilter extends ZuulFilter {
     }
 
 }
+
